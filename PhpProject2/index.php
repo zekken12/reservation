97dc1b60743571b72e3dc6f03c3c57689e82e63a
zekken12 <86075677+zekken12@users.noreply.github.com>
2022-03-09 15:26:04 +0100

    Add files via upload

---
+++ PhpProject2/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+  
+<head>
+    <title>GFG- Store Data</title>
+</head>
+  
+<body>
+    <center>
+        <h1>Storing Form data in Database</h1>
+        <br />
+        <br />
+        <a href="submit.php" >Formulaire</a>
+        <br />
+        <br />
+        <a href="delete.php" >Suppression</a>
+
+        <br />
+        <br />
+    </center>
+</body>
+  
+</html>
